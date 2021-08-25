8b0b8b110df7868a67e02249e6b6e0e52250d2fc
turing <rayaneplaterof@gmail.com>
2021-08-25 19:53:52 -0300

    Commit inicial

---
+++ src/main/java/ecommerce/dgr/DgrApplication.java
@@ -0,0 +1,13 @@
+package ecommerce.dgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ecommerce/dgr/DgrApplicationTests.java
@@ -0,0 +1,13 @@
+package ecommerce.dgr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DgrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
