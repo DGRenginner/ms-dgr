f5119f470d2618e49742890c06a504849ef75b70
GiovanniTech <giovanni.web@icloud.com>
2021-10-13 21:44:20 -0300

    Adicionando tela e tentando solucionar erro de cors

--- src/main/resources/js/connection.js
+++ src/main/resources/js/connection.js
@@ -1,14 +1,48 @@
+function fazPost(url, body) {
 
+  console.log("Body=", body) 
+    let request = new XMLHttpRequest(body)
+    request.open("post", url, true)
+    request.setRequestHeader("Content-type", "application/json")
+    request.send(JSON.stringify(body))
+  
+    request.onload = function() {
+      console.log(this.responseText)
+    }
+
+    return request.responseText
+}
+ 
 function cadastraUsuario(){
+
+
+
   event.preventDefault()
-  let url = "https://ms-dgr.herokuapp.com/" 
+  let url = "https://ms-dgr.herokuapp.com/pessoas/criarPessoa" 
   let cpf_cnpj = document.getElementById("cpf_cnpj").value
   let email = document.getElementById("email").value
-  let login = document.getElementById("login").value
   let nome = document.getElementById("nome").value
   let senha = document.getElementById("senha").value
   let confirmasenha = document.getElementById("confirmasenha").value
   let telefone = document.getElementById("telefone").value
-  let tipo_acesso = document.getElementById("tipo_acesso").value
+
+  console.log(cpf_cnpj)
+  console.log(email)
+  console.log(nome)
+  console.log(senha)
+  console.log(confirmasenha)
+  console.log(telefone)
+
+  body = {
+    "cpf_cnpj": cpf_cnpj,
+    "email" : email,
+    "nome" : nome,
+    "senha" : senha,
+    "confirmasenha" : confirmasenha,
+    "telefone" : telefone,
+  }
+fazPost(url, body)
+
+
 
  }
