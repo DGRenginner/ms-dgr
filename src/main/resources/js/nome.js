91766c33a5a7052b90b560592c01c2df130fb38e
turing <rayaneplaterof@gmail.com>
2021-11-09 22:13:17 -0300

    alteracao js

--- src/main/resources/js/nome.js
+++ src/main/resources/js/nome.js
@@ -1,29 +1,35 @@
 const carregarProdutos = async () => {
 
-    const response = await fetch('https://ms-dgr.herokuapp.com/produtos');
+    const response = await fetch('http://localhost:8081/produtos')
     const dados = await response.json()
     console.log(dados)
 
     //para cada item de produto, adiciona na pagina html um template preenchido
     dados.forEach(item => {
-        const containerProdutosElement = document.getElementById("produtos")
+        const containerProdutosElement = document.getElementById("container-produtos")
+       
+        const template = document.getElementById("produto-template")
 
-        const template = document.getElementById("product-footer")
-
-        const imagemElement = document.getElementById('product-header')
-
+        //const imagemElement = document.getElementById('product-header')
+        
         const produtoElement = document.importNode(template.content, true)
 
         const itens_produto = produtoElement.querySelectorAll('span')
-        itens_produto[0].innerText = item.nome
-        itens_produto[1].innerText = item.preco
-       /* itens_produto[2].innerText = item.descricao
+        itens_produto[0].innerText = item.id
+        itens_produto[1].innerText = item.nome
+        itens_produto[2].innerText = item.descricao
         itens_produto[3].innerText = item.marca
         itens_produto[4].innerText = item.preco
-        itens_produto[5].innerText = item.urlImagem*/
+        itens_produto[5].innerText = item.urlImagem
 
         containerProdutosElement.append(produtoElement)
     });
 
     console.log(dados)
+}
+
+
+window.onload = () => {
+    carregarProdutos()
+    console.log('Iniciado')
 }
