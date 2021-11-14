8cd536656b81db42b0c0e7193a8c7b64b81734fc
turing <rayaneplaterof@gmail.com>
2021-11-14 00:52:02 -0300

    Alteração produtos

--- src/main/resources/js/main.js
+++ src/main/resources/js/main.js
@@ -14,36 +14,37 @@ function fazPost(url, body) {
 }
 
 function cadastraProduto() {
+    console.log("CADASTRA PRODUTO")
+
     event.preventDefault()
     let xhr = new XMLHttpRequest();
 
     let url = document.getElementById("urlImagem").value
     let nome = document.getElementById("nome").value
-    let preço = document.getElementById("preço").value
-    let descrição = document.getElementById("descrição").value
+    let preco = document.getElementById("preco").value
+    let descricao = document.getElementById("descricao").value
     let marca = document.getElementById("marca").value
+    let quantidade = document.getElementById("quantidade").value
 
     console.log(url)
     console.log(nome)
-    console.log(preço)
-    console.log(descrição)
+    console.log(preco)
+    console.log(descricao)
     console.log(marca)
 
     body = {
-        "url": "DANILO",
-        "nome": "NAO VALE NADA",
-        "preco": "NENHUMA",
-        "descricao": 1,
+        "nome": nome,
+        "preco": preco,
+        "descricao": descricao,
+        "marca" : marca,
         "promocao": false,
-        "urlImagem": "img.png",
-        "quantidade": 0
-
-
+        "urlImagem": "url",
+        "quantidade": quantidade
     }
     console.log(JSON.stringify(body))
     console.log(xhr)
 
-    xhr.open('POST', "https://ms-dgr.herokuapp.com/produtos/criarProduto", true);
+    xhr.open('POST', "http://localhost:8081/produtos/criarProduto", true);
     xhr.setRequestHeader("Content-type", "application/json")
     xhr.send(JSON.stringify(body));
 
